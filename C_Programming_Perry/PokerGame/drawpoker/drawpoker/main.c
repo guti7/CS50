6f027949bcf41306876e6d2f66055fdc7c159fb7
guti7 <7505855+guti7@users.noreply.github.com>
2018-03-19 09:38:36 -0700

    DrawPoker game

---
+++ C_Programming_Perry/PokerGame/drawpoker/drawpoker/main.c
@@ -0,0 +1,38 @@
+//
+//  main.c
+//  drawpoker
+//
+//  Created by Guti on 3/19/18.
+//  Copyright © 2018 ZenG. All rights reserved.
+//
+
+// Poker Program: Appendix B Absolute Beginner's Guide to C, 3rd Edition, Perry
+
+/*
+ * Plays draw poker. User can play indefinitely, betting between 1 and 5.
+ * They are dealt 5 cards and get to choose what to keep and what to replace,
+ * The new hand is reviewed and payout is given.
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdbool.h>
+
+// Prototypes
+void printGreeting(void);
+int getBet(void);
+char getSuit(int suit);
+char getRank(int rank);
+void getFirstHand(int cardRank[], int cardSuit[]);
+void gerFinalHand(int cardRank[], int cardSuit[], int finalRank[],
+                  int finalSuit[], int ranksInHand[], int suitsInHand[]);
+
+int analyzeHand(int ranksInHand[], int ranksInSuit[]);
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
