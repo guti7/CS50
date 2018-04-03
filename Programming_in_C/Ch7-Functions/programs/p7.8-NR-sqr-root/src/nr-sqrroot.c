3049134cf8506dad105f47c909a606eaf693c2fe
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-03 00:14:05 -0700

    Find square root of a number

---
+++ Programming_in_C/Ch7-Functions/programs/p7.8-NR-sqr-root/src/nr-sqrroot.c
@@ -0,0 +1,35 @@
+/*
+* Programming in C, 4th, Kochan
+* Program 7.8
+* Created by Guti on 04/02/2018.
+*
+* Calculates the square root of a value using the
+* Newton-Raphson approximation method
+*/
+#include <stdio.h>
+
+// Calculate the absolute value of a number
+float absoluteValue(float n) {
+  return n < 0? -n: n;
+}
+
+// Compute the square root of a number
+float squareRoot(float n) {
+  const float epsilon = 0.00001;
+  float guess = 1.0;
+  
+  while(absoluteValue(guess * guess - n) >= epsilon) {
+    guess = (n / guess + guess) / 2.0;
+  }
+  
+  return guess;
+}
+
+int main(void) {
+  float number = 2.0;
+  printf("squareRoot(%.2f) = %f\n", number, squareRoot(number));
+  number = 144.0;
+  printf("squareRoot(%.2f) = %f\n", number, squareRoot(number));
+  number = 17.5;
+  printf("squareRoot(%.2f) = %f\n", number, squareRoot(number));
+}
