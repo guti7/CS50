10729d07c08cc1eac0ce59d8784f12ff031fa39a
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-04 23:57:28 -0700

    Find minimum in array with size argument

--- Programming_in_C/Ch7-Functions/programs/p7.9-minimum/src/minimum.c
+++ Programming_in_C/Ch7-Functions/programs/p7.9-minimum/src/minimum.c
@@ -7,14 +7,14 @@
 */
 #include <stdio.h>
 
-#define SIZE 10
+#define SIZE 3
 
-// Finds the minium value in an array
-int minimum(int values[]){
+// Finds the minimum value in an array
+int minimum(int values[], int size){
   int minValue, i;
   
   minValue = values[0];
-  for (i = 1; i < SIZE; i++) {
+  for (i = 1; i < size; i++) {
     if (values[i] < minValue) {
       minValue = values[i];
     }
@@ -25,16 +25,24 @@ int minimum(int values[]){
 
 int main(void) {
   int scores[SIZE], i, minScore;
+  int count;
   
-  printf("Enter 10 scores\n");
+  printf("Enter %i scores:\n", SIZE);
   
   for (i = 0; i < SIZE; i++) {
     scanf (" %i", &scores[i]);
   }
   
-  minScore = minimum(scores);
-  
+  minScore = minimum(scores, SIZE);
   printf("The minimum score is %i\n", minScore);
   
+  int array[5] = {157, -28, -37, 26, 10};
+  int array2[7] = {12, 45, 1, 10, 5, 3, 22};
+  
+  count = sizeof(array) / sizeof(array[0]);
+  printf("array  min: %i\n", minimum(array, count));
+  count = sizeof(array2) / sizeof(array2[0]);
+  printf("array2 min: %i\n", minimum(array2, count));
+    
   return 0;
 }
