b857f4a1e7e0b9685a1c504f2e0567f1940f850a
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-13 13:53:55 -0700

    Express time and increase by one second

---
+++ Programming_in_C/ch8-Structures/programs/p8.3-time/src/time.c
@@ -0,0 +1,67 @@
+/*
+* Programming in C, 4th, Kochan
+* Program 8.3
+* Created by @guti7 on 04/13/2018
+* 
+* Updates time by one second
+*/
+
+#include <stdio.h>
+
+// Time
+struct time {
+  int seconds;
+  int minutes;
+  int hours;
+};
+
+// Prototypes
+struct time updateTime(struct time t);
+void printTime(struct time t);
+
+// main
+int main(void) {
+  
+  // get start time
+  struct time currentTime, nextTime;
+  currentTime.seconds = 59;
+  currentTime.minutes = 59;
+  currentTime.hours = 23;
+  
+  // Report current time
+  printf("Current time: ");
+  printTime(currentTime);
+  
+  // Update time by second
+  printf("Next time:    ");
+  nextTime = updateTime(currentTime);
+  printTime(nextTime);
+  
+  return 0;
+}
+
+// Updates the given time by one second
+struct time updateTime(struct time t) {
+  // increase by one second 
+  t.seconds++;
+  
+  // update time members
+  if (t.seconds == 60) {
+    t.seconds = 0;
+    t.minutes++;
+    if (t.minutes == 60) {
+      t.minutes = 0;
+      t.hours++;
+      if (t.hours == 24) {
+        t.hours = 0;
+      }
+    }
+  }
+  
+  return t;
+}
+
+// Display current time to console
+void printTime(struct time t) {
+  printf("%.2i:%.2i:%.2i\n", t.hours, t.minutes, t.seconds);
+}
