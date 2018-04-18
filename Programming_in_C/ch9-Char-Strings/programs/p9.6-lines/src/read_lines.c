7ab3e0b9573b792e01cc80d712b60c3c73afb825
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-18 08:42:52 -0700

    Limit number of input characters

--- Programming_in_C/ch9-Char-Strings/programs/p9.6-lines/src/read_lines.c
+++ Programming_in_C/ch9-Char-Strings/programs/p9.6-lines/src/read_lines.c
@@ -8,32 +8,53 @@
 
 #include <stdio.h>
 
-void readLine(char buffer[]);
+#define LINE_LENGTH 80
+
+void readLine(char buffer[], int count);
+void printString(char string[]);
 
 int main(void) {
   
   int i;
   char line[81];
   
-  for (i = 0; i < 2; i++) {
-    printf("Type something: ");
-    readLine(line);
+  for (i = 0; i < 3; i++) {
+    readLine(line, LINE_LENGTH);
     printf("Typed: %s\n\n", line);
   }
+  
+  
   
   return 0;
 }
 
 // Read a line of text from the terminal
-void readLine(char buffer[]) {
+void readLine(char buffer[], int count) {
   int i = 0;
   char ch;
-  
+  printf("Type something (max %i characters): ", LINE_LENGTH);
   do {
+    // reads one character ahead
     ch = getchar();
     buffer[i] = ch;
     i++;
-  } while (ch != '\n');
+  } while (ch != '\n' && i < count);
   
-  buffer[--i] = '\0';
+  buffer[i] = '\0';
+  
+  // Clear terminal buffer before next read
+  if (i + 1 > count) {
+    while(ch != '\n' && ch != EOF) {
+      ch = getchar();
+    };
+  }
+}
+
+void printString(char string[]) {
+  int j = 0;
+  while (string[j] != '\0') {
+    printf("%c,", string[j]);
+    j++;
+  }
+  printf("\n");
 }
