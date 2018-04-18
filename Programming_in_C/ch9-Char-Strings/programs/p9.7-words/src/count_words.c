b32772ce08064b67e40df37f8cdbaf9458e03204
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-18 09:16:05 -0700

    Count words in a portion of text

---
+++ Programming_in_C/ch9-Char-Strings/programs/p9.7-words/src/count_words.c
@@ -0,0 +1,28 @@
+/*
+* Programming in C, 4th, Kochan
+* Program 9.7
+* Created by @guti7 on 04/18/2018
+*
+* Counts the words in a portion of text.
+*/
+
+#include <stdio.h>
+
+int countWords(char string[]);
+
+int main(void) {
+  
+  return 0;
+}
+
+// Returns the number of words in given string.
+// word: a sequence of one or more alphabetic characters.
+int countWords(char string[]) {
+  // Scan for the first occurance of alpha character, 
+  // and considers all subsequent characters up to the first non-alphabetic
+  // character as part of the same word. Repeats until the end of the string.
+  
+  
+   
+  return 0;
+}
