fbf13ebdba6ecd6d5a4e0587db2769d27861425c
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-18 09:56:24 -0700

    Count words in a string

--- Programming_in_C/ch9-Char-Strings/programs/p9.7-words/src/count_words.c
+++ Programming_in_C/ch9-Char-Strings/programs/p9.7-words/src/count_words.c
@@ -7,22 +7,63 @@
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int countWords(char string[]);
+int countWords(const char string[]);
+bool isAlpha(const char c);
 
 int main(void) {
+  
+  int count;
+  
+  const char string[] = "Some words";
+  const char string2[] = "Read the next words";
+  char string3[] = "Variable stringw 34234 (&)) ... again.";
+  
+  count = countWords(string);
+  printf("\"%s\": %i words\n", string, count);
+  
+  count = countWords(string2);
+  printf("\"%s\": %i words\n", string2, count);
+  
+  count = countWords(string3);
+  printf("\"%s\": %i words\n", string3, count);
   
   return 0;
 }
 
 // Returns the number of words in given string.
 // word: a sequence of one or more alphabetic characters.
-int countWords(char string[]) {
+int countWords(const char string[]) {
   // Scan for the first occurance of alpha character, 
   // and considers all subsequent characters up to the first non-alphabetic
   // character as part of the same word. Repeats until the end of the string.
+  int wordCount = 0;
   
+  int i = 0;
+  char curr;
+  bool inWord;
   
+  do {
+    curr = string[i];
+    if (isAlpha(curr)) {
+      inWord = true;
+      while(isAlpha(curr)) {
+        i++;
+        curr = string[i];
+      }
+      wordCount++;
+    } else {
+      i++;
+      curr = string[i];
+    }
+    inWord = false;
+  } while(curr != '\0');
    
-  return 0;
+  return wordCount;
+}
+
+// Checks for alphabet character
+bool isAlpha(const char c) {
+  return (c >= 65 && c <= 90) || (c >= 97 && c <= 122);
 }
