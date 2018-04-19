99b11013d64049d621f389ab02201287d9ed568a
guti7 <7505855+guti7@users.noreply.github.com>
2018-04-18 22:09:01 -0700

    Define struct and dictionary

--- Programming_in_C/ch9-Char-Strings/programs/p9.9-dictionary/src/dictionary.c
+++ Programming_in_C/ch9-Char-Strings/programs/p9.9-dictionary/src/dictionary.c
@@ -5,3 +5,22 @@
 *
 * Simulate a dictionary and lookup words to get a definition
 */
+
+struct entry {
+  char word[15];
+  char definition[50];
+};
+
+
+int main(void) {
+  
+  const struct entry dictionary[5] = 
+    {
+      { "abyss", "a bottomless pit" },
+      { "aerie", "a high nest" },
+      { "agar",  "a jelly made from seaweed" },
+      { "ajar",  "partially open" },
+    };
+  
+  return 0;
+}
